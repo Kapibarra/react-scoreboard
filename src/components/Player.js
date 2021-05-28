5b67f2743bfba48827e5862fef608fd8e5e2f08c
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-05-28 16:58:17 +0300

    fix

--- src/components/Player.js
+++ src/components/Player.js
@@ -2,8 +2,12 @@ import React, { Component } from 'react'
 
 const Player = () => {
     return (
-<div>Имя</div>
-
+        <div className="PlayerWrapper">
+<div className="PlayerName">Имя</div>
+<button>+</button>
+<p>5</p>
+<button>-</button>
+</div>
     )
 }
 export default Player
--- src/hoc/Layout.js
+++ src/hoc/Layout.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react' 
+import Player from '../components/Player'
 import classes from './layout.module.css'
 
 class Layout extends Component {
     render () {
         return (
             <div className={classes.Layout}>
+            <Player />
             </div>
         )
     }
